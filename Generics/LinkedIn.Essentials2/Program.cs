14948a5a4f209e0e8911e0d758ff2d1f4540c2d2
Dickson Souza <Dickson.Souza@rhimagnesita.com>
2023-10-21 20:06:12 -0300

    Instantiated two objects of Person type to test Swap behavior and also added reference to library for this to work

--- Generics/LinkedIn.Essentials2/Program.cs
+++ Generics/LinkedIn.Essentials2/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System.Diagnostics;
 
+using Essentials2.Library;
+
 namespace LinkedIn.Essentials2;
 
 public static class Program
 {
     public static void Main()
     {
+        // Value type variables
         int x = 5, y = 7;
 
         Console.WriteLine($"Before swapping >>> X: {x} and Y: {y}");
@@ -17,6 +20,27 @@ public static class Program
         // Expected output:
         //Before swapping >>> X: 5 and Y: 7
         //After swapping >>> X: 5 and Y: 7
+
+        // Reference type variables
+        var p1 = new Person
+        {
+            FirstName = "matt",
+            LastName = "milner",
+            Age = 50
+        };
+
+        var p2 = new Person
+        {
+            FirstName = "amanda",
+            LastName = "owner",
+            Age = 39
+        };
+
+        Console.WriteLine($"Before swapping >>> p1: {p1.ToString()} and p2: {p2.ToString()}");
+
+        Swap(p1, p2);
+
+        Console.WriteLine($"After swapping >>> p1: {p1.ToString()} and p2: {p2.ToString()}");
     }
 
     public static void Swap(object first, object second)
