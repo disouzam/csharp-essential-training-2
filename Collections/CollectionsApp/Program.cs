3a45a685fa27f2d2c896a0a3da15da99b702623f
Dickson Souza <Dickson.Souza@rhimagnesita.com>
2023-10-23 21:37:27 -0300

    Simple example of using array list with an initial size defined at creation but limit exceeded without issue

--- Collections/CollectionsApp/Program.cs
+++ Collections/CollectionsApp/Program.cs
@@ -10,6 +10,19 @@
             names[1] = "milner";
 
             Console.WriteLine("Hello {0} {1}", names);
+
+            var al = new System.Collections.ArrayList(2);
+            al.Add("First");
+            al.AddRange(new string[] { "Second", "Third", "Fourth" });
+
+            Console.WriteLine($"Collection size is {al.Count}");
+            Console.WriteLine("Indexed item from collection [2]: {0}", al[2]);
+            Console.WriteLine("All items in the list:");
+
+            foreach (var item in al)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
